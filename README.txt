bbee0bf009a0c799fefd4587227f3dd6d89124cd
Richard Neumann <r.neumann@homeinfo.de>
2014-10-16 17:34:46 +0200

    Added metadata

---
+++ README.txt
@@ -0,0 +1,11 @@
+#####################################
+#									#
+#	Immobit™ online exposé agent	#
+#									#
+#####################################
+
+LICENSE
+	Copyright (C) 2014:	HOMEINFO - Digitale Informationssysteme GmbH
+	All rights reserved.
+	
+	Contact:	Richard Neumann <mail@richard-neumann.de>
---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+	name='immobit',
+	version='0.0.1-indev',
+	author='Richard Neumann',
+	author_email='mail@richard-neumann.de',
+    requires=['his'],
+	package_dir = {'his.services': ''},
+	packages=['his.services.immobit'],
+	license=open('LICENSE.txt').read(),
+	description='An online real estate exposé tool',
+	long_description=open('README.txt').read(),
+)
