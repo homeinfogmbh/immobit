4bebfbd8cad78f0d0e5920e859be6d4dd24c662c
Richard Neumann <r.neumann@homeinfo.de>
2016-01-18 14:24:49 +0100

    Fixed for PEP8

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 
 from distutils.core import setup
 
-setup(name='homeinfo-his-immobit',
-      version='0.0.1-indev',
-      author='Richard Neumann',
-      author_email='r.neumann@homeinfo.de',
-      requires=['homeinfo.his'],
-      package_dir={'homeinfo.his': ''},
-      packages=['homeinfo.his.immobit'],
-      license=open('LICENSE.txt').read(),
-      description='An online real estate exposé tool',
-      long_description=open('README.txt').read(),
-      )
+setup(
+    name='homeinfo-his-immobit',
+    version='0.0.1-indev',
+    author='Richard Neumann',
+    author_email='r.neumann@homeinfo.de',
+    requires=['homeinfo.his'],
+    package_dir={'homeinfo.his': ''},
+    packages=['homeinfo.his.immobit'],
+    license=open('LICENSE.txt').read(),
+    description='An online real estate exposé tool',
+    long_description=open('README.txt').read())
