9c07538906d77022379011c8724ef92798e55407
Richard Neumann <r.neumann@homeinfo.de>
2017-11-20 14:19:16 +0100

    Updated author info.

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ from distutils.core import setup
 setup(
     name='immobit',
     version='latest',
-    author='Richard Neumann',
+    author='HOMEINFO - Digitale Informationssysteme GmbH',
+    author_email='<info at homeinfo dot de>',
+    maintainer='Richard Neumann',
+    maintainer_email='<r dot neumann at homeinfo period de>',
     requires=['his'],
     packages=['immobit'],
     data_files=[
