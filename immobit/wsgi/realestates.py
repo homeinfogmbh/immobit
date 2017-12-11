7803f83f8f7dda76fbdc11a70f137cda934074e2
Richard Neumann <r.neumann@homeinfo.de>
2017-12-11 11:20:04 +0100

    Added patch to reply.

--- immobit/wsgi/realestates.py
+++ immobit/wsgi/realestates.py
@@ -228,7 +228,9 @@ def patch_real_estate(ident):
     with _transaction('UPDATE', real_estate.objektnr_extern) as log:
         if _patch_real_estate(real_estate, DATA.json):
             log.success = True
-            return OK('Real estate patched.', patch=DATA.json)
+            return JSON({
+                'message': 'Real estate patched.',
+                'patch': DATA.json})
 
         raise JSON({
             'message': 'Could not patch real estate.',
