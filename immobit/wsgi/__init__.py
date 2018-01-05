77819b93492a605ca4e8fa17664c4a7c42db56f7
Richard Neumann <r.neumann@homeinfo.de>
2018-01-05 12:08:36 +0100

    Fixed dict merging.

--- immobit/wsgi/__init__.py
+++ immobit/wsgi/__init__.py
@@ -7,6 +7,10 @@ from immobit.wsgi import attachments, contacts, portals, realestates
 __all__ = ['APPLICATION']
 
 
+ENDPOINTS = {}
+ENDPOINTS.update(attachments.ENDPOINTS)
+ENDPOINTS.update(contacts.ENDPOINTS)
+ENDPOINTS.update(portals.ENDPOINTS)
+ENDPOINTS.update(realestates.ENDPOINTS)
 APPLICATION = Application('ImmoBit', debug=True, cors=True)
-APPLICATION.add_endpoints({}.update(attachments.ROUTES).update(
-    contacts.ROUTES).update(portals.ROUTES).update(realestates.ROUTES))
+APPLICATION.add_endpoints(ENDPOINTS)
