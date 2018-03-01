5da1489088500c9cfdb2037b01ec1d8c738604f6
Richard Neumann <r.neumann@homeinfo.de>
2018-03-01 14:38:51 +0100

    Removed trailing slash.

--- immobit/wsgi/realestates.py
+++ immobit/wsgi/realestates.py
@@ -243,6 +243,6 @@ def patch(ident):
 ROUTES = (
     ('GET', '/realestates', lst, 'list_real_estates'),
     ('GET', '/realestates/<int:ident>', get, 'get_real_estate'),
-    ('POST', '/realestates/', add, 'add_real_estate'),
+    ('POST', '/realestates', add, 'add_real_estate'),
     ('DELETE', '/realestates/<int:ident>', delete, 'delete_real_estate'),
     ('PATCH', '/realestates/<int:ident>', patch, 'patch_real_estate'))
