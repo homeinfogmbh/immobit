7a54e927fccb9b77f76aefa1b8bf47e3d78f1ca7
Richard Neumann <r.neumann@homeinfo.de>
2018-06-20 13:25:29 +0200

    Migrated to gettext.

--- immobit/messages.py
+++ immobit/messages.py
@@ -26,97 +26,97 @@ __all__ = [
     'ForeignAttachmentAccess']
 
 
-class RealEstatesMessage(Message):
+class ImmoBitMessage(Message):
     """Basic real estates message."""
 
-    LOCALES = '/etc/his.d/locale/immobit.ini'
+    DOMAIN = 'immobit'
 
 
-class InvalidRealEstateID(RealEstatesMessage):
+class InvalidRealEstateID(ImmoBitMessage):
     """Indicates that the provided data is not a valid real estate ID."""
 
     STATUS = 422
 
 
-class NoSuchRealEstate(RealEstatesMessage):
+class NoSuchRealEstate(ImmoBitMessage):
     """Indicates that the requested real estate does not exist."""
 
     STATUS = 404
 
 
-class RealEstatedCreated(RealEstatesMessage):
+class RealEstatedCreated(ImmoBitMessage):
     """Indicates that the real estate was successfully created."""
 
     STATUS = 201
 
 
-class CannotAddRealEstate(RealEstatesMessage):
+class CannotAddRealEstate(ImmoBitMessage):
     """Indicates that the respective real estate could not be added."""
 
     STATUS = 500
 
 
-class RealEstateExists(RealEstatesMessage):
+class RealEstateExists(ImmoBitMessage):
     """Indicates that the respective real estate already exists."""
 
     STATUS = 409
 
 
-class NoRealEstateSpecified(RealEstatesMessage):
+class NoRealEstateSpecified(ImmoBitMessage):
     """Indicates that no real estate was specified."""
 
     STATUS = 400
 
 
-class CannotDeleteRealEstate(RealEstatesMessage):
+class CannotDeleteRealEstate(ImmoBitMessage):
     """Indicates that the respective real estate could not be deleted."""
 
     STATUS = 500
 
 
-class NoRealEstateDataProvided(RealEstatesMessage):
+class NoRealEstateDataProvided(ImmoBitMessage):
     """Indicates that real estate data was expected but not provieded."""
 
     STATUS = 400
 
 
-class RealEstateUpdated(RealEstatesMessage):
+class RealEstateUpdated(ImmoBitMessage):
     """Indicates that the real estate has been updated."""
 
     STATUS = 200
 
 
-class RealEstateDeleted(RealEstatesMessage):
+class RealEstateDeleted(ImmoBitMessage):
     """Indicates that the real estate has been deleted."""
 
     STATUS = 200
 
 
-class NoAttachmentSpecified(RealEstatesMessage):
+class NoAttachmentSpecified(ImmoBitMessage):
     """Indicates that no attachment was specified."""
 
     STATUS = 400
 
 
-class NoSuchAttachment(RealEstatesMessage):
+class NoSuchAttachment(ImmoBitMessage):
     """Indicates that the requested attachment does not exist."""
 
     STATUS = 404
 
 
-class AttachmentCreated(RealEstatesMessage):
+class AttachmentCreated(ImmoBitMessage):
     """Indicates that the attachment was successfully created."""
 
     STATUS = 201
 
 
-class AttachmentDeleted(RealEstatesMessage):
+class AttachmentDeleted(ImmoBitMessage):
     """Indicates that the attachment has been deleted."""
 
     STATUS = 200
 
 
-class NoDataForAttachment(RealEstatesMessage):
+class NoDataForAttachment(ImmoBitMessage):
     """Indicates that the requested attachment
     does not yet have data stored.
     """
@@ -124,19 +124,19 @@ class NoDataForAttachment(RealEstatesMessage):
     STATUS = 412
 
 
-class AttachmentExists(RealEstatesMessage):
+class AttachmentExists(ImmoBitMessage):
     """Indicates that the respective attachment already exists."""
 
     STATUS = 409
 
 
-class AttachmentLimitExceeded(RealEstatesMessage):
+class AttachmentLimitExceeded(ImmoBitMessage):
     """Indicates that the limit for attachments has been exceeded."""
 
     STATUS = 403
 
 
-class ForeignAttachmentAccess(RealEstatesMessage):
+class ForeignAttachmentAccess(ImmoBitMessage):
     """Indicates an attempted access of a foreign attachment."""
 
     STATUS = 403
