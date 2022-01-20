f17123ab60432ff033761afca7ad7695362faf99
Richard Neumann <r.neumann@homeinfo.de>
2022-01-20 16:15:27 +0100

    Refactor indentation

--- immobit/wsgi/__init__.py
+++ immobit/wsgi/__init__.py
@@ -12,4 +12,5 @@ __all__ = ['APPLICATION']
 
 APPLICATION = Application('ImmoBit', debug=True)
 APPLICATION.add_routes(chain(
-    attachments.ROUTES, contacts.ROUTES, portals.ROUTES, realestates.ROUTES))
+    attachments.ROUTES, contacts.ROUTES, portals.ROUTES, realestates.ROUTES
+))
