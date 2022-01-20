84162784e948053f6c98772141ce3af7dd8cb6ce
Richard Neumann <r.neumann@homeinfo.de>
2022-01-20 16:07:18 +0100

    Migrate to setuptools

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='immobit',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='<info at homeinfo dot de>',
+    author_email='<info@homeinfo.de>',
     maintainer='Richard Neumann',
-    maintainer_email='<r dot neumann at homeinfo period de>',
-    requires=['his'],
+    maintainer_email='<r.neumann@homeinfo.de>',
+    install_requires=[
+        'flask',
+        'his',
+        'mdb',
+        'openimmodb',
+        'peewee',
+        'peeweeplus',
+        'wsgilib'
+    ],
     packages=['immobit', 'immobit.wsgi'],
-    description='Immobit')
+    description='Immobit'
+)
