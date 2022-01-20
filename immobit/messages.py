9519221deb47a7f2ffcf1fb8a37e4ccbd83f23e7
Richard Neumann <r.neumann@homeinfo.de>
2022-01-20 16:14:55 +0100

    Refactor indentation

--- immobit/messages.py
+++ immobit/messages.py
@@ -21,24 +21,35 @@ __all__ = [
 
 INVALID_REAL_ESTATE_ID = JSONMessage('Invalid real estate ID.', status=422)
 NO_SUCH_REAL_ESTATE = JSONMessage(
-    'The requested real estate does not exist.', status=404)
+    'The requested real estate does not exist.', status=404
+)
 REAL_ESTATE_CREATED = JSONMessage(
-    'The real estate has been created.', status=201)
+    'The real estate has been created.', status=201
+)
 CANNOT_ADD_REAL_ESTATE = JSONMessage(
-    'Could not add the real estate.', status=500)
+    'Could not add the real estate.', status=500
+)
 REAL_ESTATE_EXISTS = JSONMessage(
-    'The real estate already exists.', status=409)
+    'The real estate already exists.', status=409
+)
 CANNOT_DELETE_REAL_ESTATE = JSONMessage(
-    'The real estate could not be deleted.', status=500)
+    'The real estate could not be deleted.', status=500
+)
 REAL_ESTATE_UPDATED = JSONMessage(
-    'The real estate has been updated.', status=200)
+    'The real estate has been updated.', status=200
+)
 REAL_ESTATE_DELETED = JSONMessage(
-    'The real estate has been deleted.', status=200)
+    'The real estate has been deleted.', status=200
+)
 NO_SUCH_ATTACHMENT = JSONMessage(
-    'The requested attachment does not exist.', status=404)
+    'The requested attachment does not exist.', status=404
+)
 ATTACHMENT_CREATED = JSONMessage(
-    'The attachment has been created.', status=201)
+    'The attachment has been created.', status=201
+)
 ATTACHMENT_DELETED = JSONMessage(
-    'The attachment has been deleted.', status=200)
+    'The attachment has been deleted.', status=200
+)
 ATTACHMENT_LIMIT_EXCEEDED = JSONMessage(
-    'You have reached your attachment quota.', status=403)
+    'You have reached your attachment quota.', status=403
+)
